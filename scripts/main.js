c1e3905978095264a82f123aaeb5e35cd8f910e0
vadimka668 <102894248+vadimka668@users.noreply.github.com>
2022-04-03 12:43:12 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+//const botton = document.querySelector(".botton1");
+/* const button2 = document.querySelector(".botton2");
+const text =  document.querySelector(".text");
+const img = document.querySelector("img") 
+let textP = false;
+let imgT = false;
+
+botton.addEventListener('click', function(){
+    if (textP === true){
+        text.textContent = "world hello";
+        botton.textContent = "hello world";
+    }else{
+        text.textContent = "hello world";
+        botton.textContent = "wold hello";
+    }
+    textP = ! textP;
+});
+
+
+button2.addEventListener('click', function(){
+    if (imgT === true){
+        img.classList.remove("hidingImg")
+        text.classList.add("appearanceImg")
+        button2.textContent = "показать";
+    }else{
+        img.classList.add("hidingImg")
+        text.classList.remove("appearanceImg")
+        button2.textContent = "скрыть";
+
+    }
+    imgT = ! imgT;
+}); */
+//text.classList.remove("text2")
+//text.classList.add("text1")
+
+
+
